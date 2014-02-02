27697f0b48fa0fc4c451215f99b9c6506f312ed6
rikeshkarki <rikesh.karki@sproutify.com>
2014-02-02 11:47:35 +0545

    Initialize Blog Rails App.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '5258ad39ec08ffb0cf53ce01c3a50d3d2eb13fe9ecf4a3ca546ce5f3e767f7f45bbb0da4b3267cf35ab70125a1cf22afd0f1d384b00c96815d1cdd190710285e'
